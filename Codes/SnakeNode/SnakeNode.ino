835eb8704027455c6038ec9a9e28528c145a4696
rrishabh23 <37669747+rrishabh23@users.noreply.github.com>
2020-05-19 20:50:10 +0530

    Add files via upload

---
+++ Codes/SnakeNode/SnakeNode.ino
@@ -0,0 +1,99 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "ESPap";
+const char *password = "thereisnospoon";
+
+ESP8266WebServer server(80);
+
+int forwardPin = D0;  // Remote control movement pins
+int reversePin = D1;
+int rightPin = D2;
+int leftPin = D3;
+
+/* Just a little test message.  Go to http://192.168.4.1 in a web browser
+ * connected to this access point to see it.
+ */
+void handleRoot() {
+	server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+void fw() {
+  digitalWrite(forwardPin, HIGH);
+  digitalWrite(reversePin, LOW);
+  digitalWrite(rightPin, LOW);
+  digitalWrite(leftPin, LOW);
+  Serial.println("FW");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+void bw() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(reversePin, HIGH);
+  digitalWrite(rightPin, LOW);
+  digitalWrite(leftPin, LOW);
+  Serial.println("BW");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+void lt() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(reversePin, LOW);
+  digitalWrite(rightPin, LOW);
+  digitalWrite(leftPin, HIGH);
+  Serial.println("LT");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+void rt() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(reversePin, LOW);
+  digitalWrite(rightPin, HIGH);
+  digitalWrite(leftPin, LOW);
+  Serial.println("RT");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+void st() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(reversePin, LOW);
+  digitalWrite(rightPin, LOW);
+  digitalWrite(leftPin, LOW);
+  Serial.println("ST");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+
+	delay(1000);
+	Serial.begin(9600);
+	Serial.println();
+	Serial.print("Configuring access point...");
+	WiFi.softAP(ssid, password);
+
+	IPAddress myIP = WiFi.softAPIP();
+	Serial.print("AP IP address: ");
+	Serial.println(myIP);
+	server.on("/", handleRoot);
+  server.on("/forward", fw);
+  server.on("/backward", bw);
+  server.on("/left", lt);
+  server.on("/right", rt);
+  server.on("/stop", st);
+	server.begin();
+	Serial.println("HTTP server started");
+
+  // Set movement pins as outputs
+  pinMode(forwardPin, OUTPUT);
+  pinMode(reversePin, OUTPUT);
+  pinMode(rightPin, OUTPUT);
+  pinMode(leftPin, OUTPUT);
+  
+// Set movement pins to low
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(reversePin, LOW);
+  digitalWrite(rightPin, LOW);
+  digitalWrite(leftPin, LOW);
+}
+
+void loop() {
+	server.handleClient();
+}
---
+++ Simulation/Serpentine_Simulation/Ser_Simulation.ino
@@ -0,0 +1,71 @@
+#include <Servo.h> 
+Servo s1; 
+Servo s2;
+Servo s3;
+Servo s4; 
+Servo s5;
+Servo s6;
+Servo s7;
+Servo s8;
+Servo s9; 
+Servo s10;
+Servo s11;
+Servo s12;
+
+int counter = 0; // Loop counter variable
+float lag = .5712; // Phase lag between segments
+int frequency = 1; // Oscillation frequency of segments.
+int amplitude = 40; // Amplitude of the serpentine motion of the snake
+int rightOffset = 5; // Right turn offset
+int leftOffset = -5; // Left turn offset
+int offset = 6; // Variable to correct servos that are not exactly centered
+int delayTime = 3; // Delay between limb movements
+int test = -3; // Test varialble takes values from -6 to +5
+
+void setup() {
+   s1.attach(2);
+   s2.attach(3);
+   s3.attach(4);
+   s4.attach(5);
+   s5.attach(6);
+   s6.attach(7);
+   s7.attach(8);
+   s8.attach(9);
+   s9.attach(10);
+   s10.attach(11);
+   s11.attach(12);
+   s12.attach(13);
+ 
+   s1.write(90+amplitude*cos(5*lag));
+   s2.write(90+amplitude*cos(4*lag)); 
+   s3.write(90+amplitude*cos(3*lag));
+   s4.write(90+amplitude*cos(2*lag));
+   s5.write(90+amplitude*cos(1*lag));
+   s6.write(90+amplitude*cos(0*lag));
+   s7.write(90+amplitude*cos(-1*lag));
+   s8.write(90+amplitude*cos(-2*lag));
+   s9.write(90+amplitude*cos(-3*lag));
+   s10.write(90+amplitude*cos(-4*lag));
+   s11.write(90+amplitude*cos(-5*lag));
+   s12.write(90+amplitude*cos(-6*lag));
+
+}
+
+void loop() {
+  for(counter = 0; counter < 360; counter += 1)  {
+      delay(delayTime);
+      s1.write(90+amplitude*cos(frequency*counter*3.14159/180+5*lag));
+      s2.write(90+amplitude*cos(frequency*counter*3.14159/180+4*lag));
+      s3.write(90+amplitude*cos(frequency*counter*3.14159/180+3*lag));
+      s4.write(90+amplitude*cos(frequency*counter*3.14159/180+2*lag));
+      s5.write(90+amplitude*cos(frequency*counter*3.14159/180+1*lag));
+      s6.write(90+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
+      s7.write(90+amplitude*cos(frequency*counter*3.14159/180-1*lag));
+      s8.write(90+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
+      s9.write(90+amplitude*cos(frequency*counter*3.14159/180-3*lag));
+      s10.write(90+amplitude*cos(frequency*counter*3.14159/180-4*lag));
+      s11.write(90+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
+      s12.write(90+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
+    }
+
+}
